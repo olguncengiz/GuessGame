1236ba2ad355a3db35a921168bebc3d9ac46d202
Olgun Cengiz <olgun.cengiz@gmail.com>
2017-02-20 11:10:10 +0300

    Added difficulty

--- guess_game.py
+++ guess_game.py
@@ -1,11 +1,12 @@
 import random
 
 class GuessGame:
-    def __init__(self, length):
+    def __init__(self, length, difficulty):
         self.length = length
         self.passcode = ''
         self.steps = 1
         self.generate()
+        self.easy = difficulty
 
     def generate(self):
         numbers = '0123456789'
@@ -23,22 +24,54 @@ class GuessGame:
             elif len(guess) != len(self.passcode):
                 print 'N/A'
             else:
-                result = ''
-                numbers = dict()
-                for i in range(len(self.passcode)):
-                    if guess[i] == self.passcode[i]:
-                        result = result + guess[i]
-                    else:
-                        result = result + ' '
-                        if self.passcode[i] in numbers:
-                            numbers[self.passcode[i]] = numbers[self.passcode[i]] + 1
-                        else:
-                            numbers[self.passcode[i]] = 1
-
-                for i in range(len(result)):
-                    if result[i] == ' ' and guess[i] in numbers and numbers[guess[i]] > 0:
-                        result = result[:i] + '*' + result[i + 1:]
-                        numbers[guess[i]] = numbers[guess[i]] - 1
+                result = self.mask(guess)
                 self.steps = self.steps + 1
                 print result
+
+    def mask(self, guess):
+        result = ''
+
+        if self.easy:
+            numbers = dict()
+            for i in range(len(self.passcode)):
+                if guess[i] == self.passcode[i]:
+                    result = result + guess[i]
+                else:
+                    result = result + ' '
+                    if self.passcode[i] in numbers:
+                        numbers[self.passcode[i]] = numbers[self.passcode[i]] + 1
+                    else:
+                        numbers[self.passcode[i]] = 1
+
+            for i in range(len(result)):
+                if result[i] == ' ' and guess[i] in numbers and numbers[guess[i]] > 0:
+                    result = result[:i] + '*' + result[i + 1:]
+                    numbers[guess[i]] = numbers[guess[i]] - 1
+        else:
+            correct = 0
+            misplaced = 0
+
+            numbers = dict()
+            for i in range(len(self.passcode)):
+                if guess[i] == self.passcode[i]:
+                    correct += 1
+                    result = result + guess[i]
+                else:
+                    result = result + ' '
+                    if self.passcode[i] in numbers:
+                        numbers[self.passcode[i]] = numbers[self.passcode[i]] + 1
+                    else:
+                        numbers[self.passcode[i]] = 1
+
+            for i in range(len(result)):
+                if result[i] == ' ' and guess[i] in numbers and numbers[guess[i]] > 0:
+                    misplaced += 1
+                    numbers[guess[i]] = numbers[guess[i]] - 1
+
+            result = 'Correct = %s\nMisplaced = %s\n' % (correct, misplaced)
+
+        return result
+
+
+
                 
